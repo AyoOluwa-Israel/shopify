bbdb37b6747c19aaa0f1235cae5befb185e9b68c
AyoOluwa-Israel <izzyfresher@gmail.com>
2021-01-09 21:18:03 +0100

    Initialize project using Create React App

---
+++ src/context.js
@@ -0,0 +1,13 @@
+import React, { useState, useContext, useEffect } from 'react'
+
+import { useCallback } from 'react'
+
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) = {
+
+  return <AppContext.Provider>
+
+
+}
